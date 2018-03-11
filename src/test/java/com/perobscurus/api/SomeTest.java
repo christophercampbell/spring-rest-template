fdffcd98e64a3f710baf970a0dd6e80b648c28a2
Campbell, Christopher <Christopher.Campbell@team.neustar>
2018-03-11 10:51:36 -0400

    add junit stub

---
+++ src/test/java/com/perobscurus/api/SomeTest.java
@@ -0,0 +1,12 @@
+package com.perobscurus.api;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SomeTest {
+
+    @Test
+    public void testIt() {
+        Assert.assertEquals("an integer equals itself", true, 1 == 1);
+    }
+}
