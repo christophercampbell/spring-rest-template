dc8fb534b9c3b39f488a93926b36ccdfe0cc7acd
Campbell, Christopher <Christopher.Campbell@team.neustar>
2018-03-13 20:30:59 -0400

    unused import

--- src/main/java/com/perobscurus/api/controllers/FileUploadController.java
+++ src/main/java/com/perobscurus/api/controllers/FileUploadController.java
@@ -6,13 +6,14 @@ import com.perobscurus.api.messages.ImmutableFileUpload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.UUID;
 
